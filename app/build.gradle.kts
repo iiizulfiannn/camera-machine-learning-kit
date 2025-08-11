8fd2bf28d2058b88c51296c226c01199b7a95879
zul <iiizulfiannn@gmail.com>
2025-08-11 10:25:40 +0700

    deps: add CameraX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,21 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.play.services.mlkit.face.detection)
+    implementation(libs.play.services.vision.common)
+    implementation(libs.androidx.camera.mlkit.vision)
+    implementation(libs.mlkit.vision.common)
+    implementation(libs.androidx.camera.extensions)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
