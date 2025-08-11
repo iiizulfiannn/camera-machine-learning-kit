e2b4c03573f3a7b13c40bec159ca2e6523cea5d9
zul <iiizulfiannn@gmail.com>
2025-08-11 11:24:19 +0700

    feat: add setting camera permission

--- app/src/main/java/com/luckyfriday/cameramlkit/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/cameramlkit/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.luckyfriday.cameramlkit
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luckyfriday.cameramlkit.databinding.ActivityMainBinding
@@ -16,5 +20,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+        askCameraPermission()
+    }
+
+    private fun askCameraPermission() {
+        if (arrayOf(android.Manifest.permission.CAMERA).all {
+                ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+            }) {
+
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            Toast.makeText(this, "CAMERA PERMISSION DENIED!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
